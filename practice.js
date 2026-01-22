86d2e4249caae853a12965e4fa028b3aba4aa6a9
jeweltelecom2022 <jeweltelecom2022@gmail.com>
2026-01-23 01:52:39 +0600

    first javascript commit

---
+++ practice.js
@@ -0,0 +1,26 @@
+var iHave = 1000;
+var orangePrice = 300;
+var applePrice = 400;
+var totalOrangePrice = orangePrice * 1;
+var totalApplePrice = applePrice * 1;
+
+var totalCost = totalOrangePrice + totalApplePrice;
+var returnTaka = iHave-totalCost;
+console.log(returnTaka);
+
+
+var mathmaticsMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+var totalMarks = mathmaticsMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+var averageMarks = totalMarks / 5;
+
+console.log(averageMarks.toFixed(2));
+
+
+var smapleNumber = 119;
+var modulusNumber = 119 % 5;
+console.log(modulusNumber);
---
+++ var.js
@@ -0,0 +1,23 @@
+var name = "amar mithila";
+const wifeName ="amar mithon";
+
+var age = 25;
+var newAge = 36;
+var age = 22.4;
+var age = '22.4';
+var age = parseInt('22.4');
+var age = parseFloat('22.4');
+var taka = 0.1;
+var poisa = 0.2;
+var pora = 22;
+var name = "amar mithon";
+var name = null
+
+console.log(name);
+console.log(age);
+console.log(newAge + age);
+console.log(taka + poisa);
+var totalTekaPoisa = (taka + poisa);
+console.log(totalTekaPoisa. toFixed(4));
+console.log(pora.lekha);
+console.log(name);
