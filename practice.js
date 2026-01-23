5a30c3a73b4c985c40d0024a42ba6142deb6bfe3
jeweltelecom2022 <jeweltelecom2022@gmail.com>
2026-01-23 14:25:12 +0600

    updated with all 4 practice task done

--- practice.js
+++ practice.js
@@ -1,3 +1,4 @@
+// Practice Task- 01
 var iHave = 1000;
 var orangePrice = 300;
 var applePrice = 400;
@@ -8,6 +9,7 @@ var totalCost = totalOrangePrice + totalApplePrice;
 var returnTaka = iHave-totalCost;
 console.log(returnTaka);
 
+// Practice Task- 02
 
 var mathmaticsMarks = 75.25;
 var biologyMarks = 65;
@@ -20,7 +22,22 @@ var averageMarks = totalMarks / 5;
 
 console.log(averageMarks.toFixed(2));
 
+// Practice Task- 03
 
 var smapleNumber = 119;
 var modulusNumber = 119 % 5;
 console.log(modulusNumber);
+
+// Practice Task- 04
+
+const a = isNaN("11");
+console.log(a);
+
+
+const b = isNaN(2-10);
+console.log(b);
+
+// ekhane const mane hocche eta 1 bar e decleare kora jabe, jodi same name er arekta const decleare kori tahole output error ashbe
+// kintu jodi porer const tar name change kore dei tahole result false dekhay. tar mane hocche isNaN
+// use korle braket e number thakuk kingba subtraction ,devidation,multiplicaion thakuk false dehabe 
+// sheta "" dileo number count korbe na number likhar moto korleo false e dekhabe.
